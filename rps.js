6166516d474f82154131b8b6e0c371a74a52fc7b
Daniel Hjartland <squashical@gmail.com>
2022-10-11 11:38:30 +0200

    Create game function and logic and finish player choice function

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let choiceInt = Math.floor(Math.random() * 3);
     if (choiceInt === 0) {
@@ -10,11 +13,52 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
+
 function getPlayerChoice() {
-    let playerChoice = prompt("Please choose: rock, paper or scissors.")
-    return playerChoice
+    let playerInputChoice, playerChoice, playerFinalChoice;
+    do {
+        playerInputChoice = prompt("Please choose: rock, paper or scissors.");
+        playerChoice = playerInputChoice.toLowerCase();
+    } while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
+    if (playerChoice === "rock"){
+        playerFinalChoice = "Rock"
+    } else if (playerChoice === "paper"){
+        playerFinalChoice = "Paper"
+    } else {
+        playerFinalChoice = "Scissors"
+    }
+    return playerFinalChoice;
 }
-let computerSelection = getComputerChoice();
-console.log(computerSelection);
-let playerSelection = getPlayerChoice();
-console.log(playerSelection);
+
+function playRound(computerSelection, playerSelection) {
+    let roundString;
+    if (computerSelection === playerSelection) {
+        roundString = "Draw! " + computerSelection + " draws with " + playerSelection.toLowerCase() + ".";
+    } else if (
+        (playerSelection === "Rock" && computerSelection === "Scissors") || 
+        (playerSelection === "Paper" && computerSelection === "Rock") || 
+        (playerSelection === "Scissors" && computerSelection === "Paper")
+    ) {
+        roundString = "You win! " + playerSelection + " beats " + computerSelection.toLowerCase() + ".";
+        playerScore = ++playerScore
+    } else {
+        roundString = "You lose! "+ computerSelection + " beats " + playerSelection.toLowerCase() + ".";
+        computerScore = ++ computerScore
+    }
+    return roundString;
+}
+
+function game() {
+    let resultString, scoreString;
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelection = getPlayerChoice();
+        resultString = playRound(computerSelection, playerSelection);
+        alert(resultString)
+        scoreString = "Player: " + playerScore.toString() + " - Computer: " + computerScore.toString();
+        alert(scoreString)
+        
+    }
+}
+
+game();
