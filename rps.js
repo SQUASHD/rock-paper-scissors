a104479175620911076cf0ba7eb18bd6478c9850
Daniel Hjartland <squashical@gmail.com>
2022-10-13 11:57:51 +0200

    Add game logic and basic formatting

--- rps.js
+++ rps.js
@@ -1,64 +1,91 @@
 let playerScore = 0;
 let computerScore = 0;
+let roundWinner = "";
 
 function getComputerChoice() {
-    let choiceInt = Math.floor(Math.random() * 3);
-    if (choiceInt === 0) {
-        return "Rock";
-    }
-    else if (choiceInt === 1) {
-        return "Paper";
-    }
-    else {
-        return "Scissors";
-    }
+	let randomNumber = Math.floor(Math.random() * 3);
+	switch (randomNumber) {
+		case 0:
+			return 'ROCK'
+		case 1:
+			return 'PAPER'
+		case 2:
+			return 'SCISSORS'
+	}
 }
 
-function getPlayerChoice() {
-    let playerInputChoice, playerChoice, playerFinalChoice;
-    do {
-        playerInputChoice = prompt("Please choose: rock, paper or scissors.");
-        playerChoice = playerInputChoice.toLowerCase();
-    } while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
-    if (playerChoice === "rock"){
-        playerFinalChoice = "Rock"
-    } else if (playerChoice === "paper"){
-        playerFinalChoice = "Paper"
-    } else {
-        playerFinalChoice = "Scissors"
-    }
-    return playerFinalChoice;
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+const playerScorePara = document.getElementById('playerScore');
+const computerScorePara = document.getElementById('computerScore');
+const scoreMessage = document.getElementById('scoreMessage');
+
+const defaultScoreMessage = 'First to 5 points wins'
+
+rockBtn.addEventListener('click', () => handleClick('ROCK'));
+paperBtn.addEventListener('click', () => handleClick('PAPER'));
+scissorsBtn.addEventListener('click', () => handleClick('SCISSORS'));
+
+function handleClick(playerSelection) {
+	if (checkGameOver()){
+		return
+	}
+	const computerSelection = getComputerChoice()
+	playRound(computerSelection, playerSelection)
+	updateScore()
+	if (playerScore === 5) {
+		scoreMessage.textContent = 'You win!'
+		return
+	}
+	else if (computerScore ===5) {
+		scoreMessage.textContent = 'You lose!'
+		return
+	}
 }
 
 function playRound(computerSelection, playerSelection) {
-    let roundString;
-    if (computerSelection === playerSelection) {
-        roundString = "Draw! " + computerSelection + " draws with " + playerSelection.toLowerCase() + ".";
-    } else if (
-        (playerSelection === "Rock" && computerSelection === "Scissors") || 
-        (playerSelection === "Paper" && computerSelection === "Rock") || 
-        (playerSelection === "Scissors" && computerSelection === "Paper")
-    ) {
-        roundString = "You win! " + playerSelection + " beats " + computerSelection.toLowerCase() + ".";
-        playerScore = ++playerScore
-    } else {
-        roundString = "You lose! "+ computerSelection + " beats " + playerSelection.toLowerCase() + ".";
-        computerScore = ++ computerScore
-    }
-    return roundString;
+	if (computerSelection === playerSelection) {
+		roundWinner = 'tie'
+	} 
+	else if (
+		(playerSelection === "ROCK" && computerSelection === "SCISSORS") || 
+		(playerSelection === "PAPER" && computerSelection === "ROCK") || 
+		(playerSelection === "SCISSORS" && computerSelection === "PAPER")
+	) {
+		playerScore++;
+		roundWinner = 'player';
+	} 
+	else {
+		computerScore++;
+		roundWinner = 'computer'
+	}
+	updateScoreMessage(roundWinner, playerSelection, computerSelection)
 }
 
-function game() {
-    let resultString, scoreString;
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice();
-        let playerSelection = getPlayerChoice();
-        resultString = playRound(computerSelection, playerSelection);
-        alert(resultString)
-        scoreString = "Player: " + playerScore.toString() + " - Computer: " + computerScore.toString();
-        alert(scoreString)
-        
-    }
+function updateScore() {
+	playerScorePara.textContent = `Player: ${playerScore}`
+  computerScorePara.textContent = `Computer: ${computerScore}`
 }
 
-game();
+function updateScoreMessage(winner, playerSelection, computerSelection) {
+  if (winner === 'player') {
+    scoreMessage.textContent = `${playerSelection} beats ${computerSelection.toLowerCase()}`
+  }
+  else if (winner === 'computer') {
+    scoreMessage.textContent = `${playerSelection} is beaten by ${computerSelection.toLowerCase()}`
+  } 
+	else {
+		scoreMessage.textContent = `${playerSelection} ties with ${computerSelection.toLowerCase()}`
+	}
+}
+
+function checkGameOver() {
+	return playerScore === 5 || computerScore === 5
+}
+
+function resetGame() {
+	playerScore = 0;
+	computerScore = 0;
+	scoreMessage.textContent = defaultScoreMessage
+}
